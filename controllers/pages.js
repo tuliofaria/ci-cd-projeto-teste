b9dfbbca546657da4e175212e231b65be698aa07
Tulio Faria <tuliofaria@gmail.com>
2017-10-09 11:06:14 -0300

    initial version

---
+++ controllers/pages.js
@@ -0,0 +1,17 @@
+const home = (req, res) => res.send('CI/CD Project')
+const page1 = (req, res) => res.send('Page1')
+const calc = (req, res) => {
+  const num1 = parseInt(req.params.num1)
+  const num2 = parseInt(req.params.num2)
+  if(num2 === 0){
+    res.send('DivisaoZero')
+  }else{
+    if(num1%num2===0){
+      res.send('Divisivel')
+    }else{
+      res.send('NaoDivisivel')
+    }
+  }
+}
+
+module.exports = { home, page1 , calc }
