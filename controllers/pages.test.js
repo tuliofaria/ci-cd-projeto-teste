f2931380529968b3633a201eca9b4c45f064ba0d
Tulio Faria <tuliofaria@gmail.com>
2017-10-09 11:38:42 -0300

    fixing

--- controllers/pages.test.js
+++ controllers/pages.test.js
@@ -27,7 +27,7 @@ describe('Pages', () => {
     const res = {
       send: sinon.spy()
     }
-    pages.page1(req, res)
+    pages.page2(req, res)
     expect(res.send.calledOnce).to.be.true
     expect(res.send.calledWith('Page2')).to.be.true
   })
